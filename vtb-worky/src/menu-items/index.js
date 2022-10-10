27cc3b14a5906b708492d1dd43871729ff6a1c24
a_polumestny <anpolumestny@gmail.com>
2022-10-10 11:02:05 +0300

    delete web

--- vtb-worky/src/menu-items/index.js
+++
@@ -1,7 +0,0 @@
-import main from "./main"
-
-const menuItems = {
-    items: [main]
-}
-
-export default menuItems;
--- vtb-worky/src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import menu from './menu'
-
-const reducers = combineReducers({ menu })
-
-export default reducers;
